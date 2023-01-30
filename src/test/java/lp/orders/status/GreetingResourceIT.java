3a060b71d8a5294e7c7bdbd93aac9bdeb0d68f02
quarkusio <no-reply@quarkus.io>
2023-01-30 19:36:06 +0000

    Initial commit

---
+++ src/test/java/lp/orders/status/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package lp.orders.status;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
